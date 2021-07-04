083a12f320324e2a392caad6173d4296fb732749
Ic3Style <adri-ghost@hotmail.com>
2021-07-04 14:30:21 +0200

    cambios css

--- catan-react/src/components/Casilla.js
+++ catan-react/src/components/Casilla.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../assets/css/Board.css";
 
 const Casilla = (props) => {
     //const [state, setstate] = useState("");
@@ -15,14 +16,49 @@ const Casilla = (props) => {
         bosqueImage.style.top =  top - 30+'px';
         console.log(top, left)
     }
+
+    function setColor(tile){
+        let color;
+        switch(tile){
+            case "Pasture": //wool
+                color = "#CFF4AC";
+                break;
+            case "Hills": //brick
+                color = "#C28E7B"
+                break;
+            case "Mountains": //ore
+                color = "#E6E6E6"
+                break;
+            case "Fields": //grain
+                color = "#E7DC82"
+                break;
+            case "Forest": //lumber
+                color = "#31832D"
+                break;
+            case "Dessert":
+                color = "#DEDA9C"
+                break;
+            default:
+                color = "white"
+                break;        
+        }
+        return color;
+    }
     //setstate("Hola")
     //{ state}
+    
+    //DUDAS
+
+    //style={{background-color: setColor(props.tile)}}  como se pone
+    //se puede poner un if en css o html? para los colores de los numeros y el desierto
+
     return (
         <div>
-            <td className="hexagon" id={props.uniqueID} key={props.uniqueID} onClick={(e)=>{moveElement(e)}}> 
-                  {
-                      props.idNumber
-                     
+            <td className="hexagon" id={props.uniqueID}  key={props.uniqueID} onClick={(e)=>{moveElement(e)}}> 
+                  { 
+                    <div className="numberContainer"> 
+                        <b className="number">{props.idNumber}</b>
+                    </div>                     
                   }
                   
             </td>
